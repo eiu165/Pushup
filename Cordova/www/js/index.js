30a71a5d14b6cfef1c9d9a9a43d3b1215f9c5fc1
kathy <katthompson@aspenms.com>
2012-10-18 14:40:03 -0500

    add jquery

--- Cordova/www/js/index.js
+++ Cordova/www/js/index.js
@@ -18,25 +18,25 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // `load`, `deviceready`, `offline`, and `online`.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of `this` is the event. In order to call the `receivedEvent`
     // function, we must explicity call `app.receivedEvent(...);`
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
 
@@ -47,12 +47,13 @@ var app = {
 
 
 
-        var message = parentElement.querySelector('.message');
-        message.setHTML(device.name + ' Device Name ' + device.cordova + ' Cordova Version');
-//        $("#devicephonegap").html(device.phonegap);
-//        $("#deviceplatform").html(device.platform);
-//        $("#deviceuuid").html(device.uuid);
-//        $("#deviceversion").html(device.version);
+        //var message = parentElement.querySelector('.message');
+        //message.setHTML(device.name + ' Device Name ' + device.cordova + ' Cordova Version');
+        $('.message').html(device.name + 'Device Name');
+        //        $("#devicephonegap").html(device.phonegap);
+        //        $("#deviceplatform").html(device.platform);
+        //        $("#deviceuuid").html(device.uuid);
+        //        $("#deviceversion").html(device.version);
 
 
 
