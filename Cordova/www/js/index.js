8006c477ad1bf9948aae9cf79f153996ad9c544b
eiu165 <eiu165@gmail.com>
2012-10-17 22:14:46 -0500

    back to the starup to see if device is ever ready

--- Cordova/www/js/index.js
+++ Cordova/www/js/index.js
@@ -18,30 +18,23 @@
  */
 var app = {
     // Application Constructor
-    initialize: function () {
+    initialize: function() {
         this.bindEvents();
-
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // `load`, `deviceready`, `offline`, and `online`.
-    bindEvents: function () {
+    bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of `this` is the event. In order to call the `receivedEvent`
     // function, we must explicity call `app.receivedEvent(...);`
-    onDeviceReady: function () {
+    onDeviceReady: function() {
         app.receivedEvent('deviceready');
-
-        console.log(' hello '); 
-
-        $('#Output').html('navigator.camera: ' + navigator.camera);
     },
-
-    
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
@@ -52,7 +45,5 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    } 
-
+    }
 };
-
